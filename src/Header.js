829998894790d546f9bc02bec903fd9c5ba790c9
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-05-22 23:03:22 -0600

    update

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Header.css";
+
+function Header() {
+    return <div classname="header"> 
+
+
+            <div className="header__left">
+                <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/FaceB.png/120px-FaceB.png" alt=""/>
+            </div>
+
+
+            <div className="header__middle"></div>
+
+
+            <div classname="header__right"></div>
+
+            
+        </div>
+    
+}
+
+export default Header
