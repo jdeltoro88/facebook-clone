7ca4e46accd7a15eb565dc88a6d352374537bb27
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-05-24 18:37:33 -0600

    style on form

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import  "./MessageSender.css";
 import { Avatar } from "@material-ui/core";
+import VideocamIcon from '@material-ui/icons/Videocam';
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 
 function MessageSender() {
     const handleSubmit = (e) => {
@@ -22,6 +25,22 @@ function MessageSender() {
             </div>
 
             <div className='messageSender__bottom'>
+                <div className='messageSender__option'>
+                    <VideocamIcon style={{ color: "red" }}/>
+                    <h3>Live Video</h3>
+                </div>
+
+                <div className='messageSender__option'>
+                    <PhotoLibraryIcon style={{ color: "green" }}/>
+                    <h3>photo/Video</h3>
+                </div>
+
+
+                <div className='messageSender__option'>
+                    <InsertEmoticonIcon style={{ color: "orange" }}/>
+                    <h3>Feeling/Activity</h3>
+                </div>
+
 
 
             </div>
